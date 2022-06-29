10644e6d60cf97347b37fa268fce182aba478266
Blagovest <blagovest.kolev@gmail.com>
2022-06-29 10:55:29 +0300

    Initial commit

---
+++ MyForumSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyForumSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

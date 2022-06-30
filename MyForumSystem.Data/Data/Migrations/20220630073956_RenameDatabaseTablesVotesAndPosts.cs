c691f7fe422e86549a0bda87cbf39d1ea1b56d35
Blagovest <blagovest.kolev@gmail.com>
2022-06-30 10:41:51 +0300

    Rename db table Post to Posts and Vote to Votes.

---
+++ MyForumSystem.Data/Data/Migrations/20220630073956_RenameDatabaseTablesVotesAndPosts.cs
@@ -0,0 +1,255 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyForumSystem.Data.Migrations
+{
+    public partial class RenameDatabaseTablesVotesAndPosts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Post_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_AspNetUsers_CreatorId",
+                table: "Post");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Post_Categories_CategoryId",
+                table: "Post");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vote_AspNetUsers_UserId",
+                table: "Vote");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vote_Comments_CommentId",
+                table: "Vote");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vote_Post_PostId",
+                table: "Vote");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vote",
+                table: "Vote");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Post",
+                table: "Post");
+
+            migrationBuilder.RenameTable(
+                name: "Vote",
+                newName: "Votes");
+
+            migrationBuilder.RenameTable(
+                name: "Post",
+                newName: "Posts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Vote_UserId",
+                table: "Votes",
+                newName: "IX_Votes_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Vote_PostId",
+                table: "Votes",
+                newName: "IX_Votes_PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Vote_CommentId",
+                table: "Votes",
+                newName: "IX_Votes_CommentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Post_CreatorId",
+                table: "Posts",
+                newName: "IX_Posts_CreatorId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Post_CategoryId",
+                table: "Posts",
+                newName: "IX_Posts_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Votes",
+                table: "Votes",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_AspNetUsers_CreatorId",
+                table: "Posts",
+                column: "CreatorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Categories_CategoryId",
+                table: "Posts",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votes_AspNetUsers_UserId",
+                table: "Votes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votes_Comments_CommentId",
+                table: "Votes",
+                column: "CommentId",
+                principalTable: "Comments",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votes_Posts_PostId",
+                table: "Votes",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_AspNetUsers_CreatorId",
+                table: "Posts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Categories_CategoryId",
+                table: "Posts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votes_AspNetUsers_UserId",
+                table: "Votes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votes_Comments_CommentId",
+                table: "Votes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votes_Posts_PostId",
+                table: "Votes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Votes",
+                table: "Votes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts");
+
+            migrationBuilder.RenameTable(
+                name: "Votes",
+                newName: "Vote");
+
+            migrationBuilder.RenameTable(
+                name: "Posts",
+                newName: "Post");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Votes_UserId",
+                table: "Vote",
+                newName: "IX_Vote_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Votes_PostId",
+                table: "Vote",
+                newName: "IX_Vote_PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Votes_CommentId",
+                table: "Vote",
+                newName: "IX_Vote_CommentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Posts_CreatorId",
+                table: "Post",
+                newName: "IX_Post_CreatorId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Posts_CategoryId",
+                table: "Post",
+                newName: "IX_Post_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vote",
+                table: "Vote",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Post",
+                table: "Post",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Post_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Post",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_AspNetUsers_CreatorId",
+                table: "Post",
+                column: "CreatorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Post_Categories_CategoryId",
+                table: "Post",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vote_AspNetUsers_UserId",
+                table: "Vote",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vote_Comments_CommentId",
+                table: "Vote",
+                column: "CommentId",
+                principalTable: "Comments",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vote_Post_PostId",
+                table: "Vote",
+                column: "PostId",
+                principalTable: "Post",
+                principalColumn: "Id");
+        }
+    }
+}
--- MyForumSystem.Data/Data/MyForumDbContext.cs
+++ MyForumSystem.Data/Data/MyForumDbContext.cs
@@ -27,7 +27,8 @@ namespace MyForumSystem.Data
                 
         }
         public DbSet<Category>? Categories { get; set; }
-        public DbSet<Post>? Post { get; set; }
+        public DbSet<Post>? Posts { get; set; }
         public DbSet<Comment>? Comments { get; set; }
+        public DbSet<Vote> Votes { get; set; }
     }
 }
